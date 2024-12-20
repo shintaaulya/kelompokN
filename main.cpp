57c4be759731138476c694db412ad21dc180552d
shintaaulya <shintaaulya20@gmail.com>
2024-12-20 19:47:28 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+   int main() {
+    Graf G;
+    inisialisasiGraf(G);
+
+    tambahPengguna(G, "Alice", "01/01/2000", 23);
+    tambahPengguna(G, "Bob", "02/02/1999", 24);
+    tambahPengguna(G, "Charlie", "03/03/1998", 25);
+
+    tambahHubungan(G, "Alice", "Bob");
+    tambahHubungan(G, "Alice", "Charlie");
+    tambahHubungan(G, "Bob", "Charlie");
+
+    cout << "Graf pengguna:\n";
+    tampilkanGraf(G);
+
+    cariTerpopuler(G);
+    cariTidakTerpopuler(G);
+
     return 0;
 }
+
+
