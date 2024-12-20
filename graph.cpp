788ce4d57e2dad6f1ffc585c767b238767ff52da
shintaaulya <shintaaulya20@gmail.com>
2024-12-20 19:45:36 +0700

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,13 +1,14 @@
 #include "graph.h"
 
-void buatPengguna(adr*& nextVertex, string nama, string Ttl, int umur) {
-    adr = new Pengguna;
-    adr -> info.nama = nama;
-    adr -> info.Ttl = Ttl;
-    adr -> info.umur = umur;
-    adr -> nextTeman = nullptr;
-    adr -> nextPengguna = nullptr;
+void buatPengguna(Pengguna*& nextVertex, string nama, string Ttl, int umur) {
+    nextVertex = new Pengguna;
+    nextVertex->info.nama = nama;
+    nextVertex->info.Ttl = Ttl;
+    nextVertex->info.umur = umur;
+    nextVertex->nextEdge = nullptr;
+    nextVertex->nextVertex = nullptr;
 }
+
 void inisialisasiGraf(Graf &G) {
    G.first = nullptr;
 }
@@ -28,18 +29,76 @@ Pengguna searchPengguna(Graf G, string nama){
 }
 
 
-void tambahHubungan(Graf &G, string pengguna1, string pengguna2) {
-    Pengguna searchPengguna(Graf G, string nama)
-    buatPengguna(pengguna1, pengguna2, penggunaBaru);
+void tambahHubungan(Graf& G, string pengguna1, string pengguna2) {
+    Pengguna* p1 = searchPengguna(G, pengguna1);
+    Pengguna* p2 = searchPengguna(G, pengguna2);
 
-    if(G.firstPengguna == nullptr) {
-        G.firstPengguna = penggunaBaru;
-    }else{
-        Pengguna* temp = G.firstPengguna;
-        while (temp->nextVertex != nullptr) {
-            temp = temp->nextVertex;
-        }
-        temp->nextVertex = penggunaBaru;
+    if (p1 != nullptr && p2 != nullptr) {
+        Teman* temanBaru = new Teman;
+        temanBaru->nama = pengguna2;
+        temanBaru->nextEdge = p1->nextEdge;
+        p1->nextEdge = temanBaru;
+    } else {
+        cout << "Salah satu atau kedua pengguna tidak ditemukan.\n";
     }
+}
+
+void tampilkanGraf(Graf G) {
+    Pengguna* temp = G.first;
+    while (temp != nullptr) {
+        cout << "Nama: " << temp->info.nama << ", Ttl: " << temp->info.Ttl << ", Umur: " << temp->info.umur << endl;
+        Teman* edge = temp->nextEdge;
+        cout << "Teman: ";
+        while (edge != nullptr) {
+            cout << edge->nama << " ";
+            edge = edge->nextEdge;
+        }
+        cout << endl;
+        temp = temp->nextVertex;
+    }
+}
+
+void cariTerpopuler(Graf G) {
+    Pengguna* temp = G.first;
+    string terpopuler;
+    int maxHubungan = 0;
+
+    while (temp != nullptr) {
+        int count = 0;
+        Teman* edge = temp->nextEdge;
+        while (edge != nullptr) {
+            count++;
+            edge = edge->nextEdge;
+        }
+        if (count > maxHubungan) {
+            maxHubungan = count;
+            terpopuler = temp->info.nama;
+        }
+        temp = temp->nextVertex;
+    }
+
+    cout << "Pengguna terpopuler: " << terpopuler << " dengan " << maxHubungan << " hubungan." << endl;
+}
+
+void cariTidakTerpopuler(Graf G) {
+    Pengguna* temp = G.first;
+    string tidakTerpopuler;
+    int minHubungan = INT_MAX;
+
+    while (temp != nullptr) {
+        int count = 0;
+        Teman* edge = temp->nextEdge;
+        while (edge != nullptr) {
+            count++;
+            edge = edge->nextEdge;
+        }
+        if (count < minHubungan) {
+            minHubungan = count;
+            tidakTerpopuler = temp->info.nama;
+        }
+        temp = temp->nextVertex;
+    }
+
+    cout << "Pengguna tidak terpopuler: " << tidakTerpopuler << " dengan " << minHubungan << " hubungan." << endl;
 }
 
